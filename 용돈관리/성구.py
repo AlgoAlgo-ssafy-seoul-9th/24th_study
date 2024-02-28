18870f260466c5d9c2a3d2dc9793569bf7af699c
L1m3Kun <sungku2757@gmail.com>
2024-02-28 14:57:21 +0900

    [성구] 용돈관리 개선

--- 용돈관리/성구.py
+++ 용돈관리/성구.py
@@ -10,9 +10,7 @@ def check(mid:int, arr:list, N:int, M:int) -> int: # 충전 횟수 계산
             return cnt
         if K >= arr[i]:
             K -= arr[i]
-        else:
-            if arr[i] > mid:    # 만약 충전을 해도 부족하면 충전금액을 늘리기 위해 M보다 큰 값 반환
-                return M + 1    
+        else:    
             K = mid - arr[i]    
             cnt += 1
     return cnt
@@ -21,18 +19,16 @@ def check(mid:int, arr:list, N:int, M:int) -> int: # 충전 횟수 계산
 def solution():
     N, M = map(int, input().split())
     arr = [int(input()) for _ in range(N)]
-    s,e = min(arr), sum(arr)
+    s,e = max(arr), sum(arr)
     # binary search
     while s<=e:
         mid = (s+e)//2
-        m = check(mid, arr, N, M)   
-        if m == M:
-            e = mid-1
-            continue            
-        elif m < M: 
+        m = check(mid, arr, N, M)            
+        if m <= M: 
             e = mid-1
         else:
             s = mid+1
+
     # m==M 일때 e=mid-1을 한번더 하기에 1을 더해줌
     print(e+1)
 
